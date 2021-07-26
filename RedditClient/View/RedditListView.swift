9b60cff6c791e254171828e95c16e5d6d965a003
marco.reyes <marco.reyes@payclip.com>
2021-07-26 16:55:46 -0500

    first version of delete dismiss all behavior

--- RedditClient/View/RedditListView.swift
+++ RedditClient/View/RedditListView.swift
@@ -11,12 +11,22 @@ class RedditListView: UIView {
     var tableView: UITableView
     let refreshControl = UIRefreshControl() // test making this weak
 
+    lazy var dismissAllButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Dismiss All", for: .normal)
+        button.addTarget(self, action: #selector(didPressDismissAllButton), for: .touchUpInside)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = .gray
+        return button
+    }()
+
     init (frame: CGRect, dataSource: UITableViewDataSource, refreshControl: UIRefreshControl) {
         tableView = UITableView()
         super.init(frame: frame)
         tableView.register(RedditItemCell.self, forCellReuseIdentifier: RedditItemCell.reuseID)
         tableView.dataSource = dataSource
         tableView.refreshControl = refreshControl
+        backgroundColor = .white
         setupView()
     }
 
@@ -24,18 +34,23 @@ class RedditListView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
 
+    // TODO: - chnage this to call view controller in order to delete data soruce
+    @objc func didPressDismissAllButton() {
+        UIView.animate(withDuration: 0.5, delay: 0.0, options: [], animations: {
+            self.tableView.alpha = 0.0
+            self.tableView.transform = .identity
+        })
+    }
+
     private func setupView() {
         tableView.estimatedRowHeight = 88
         tableView.rowHeight = 160
         tableView.separatorColor = .lightGray
         addSubview(tableView)
-        tableView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(dismissAllButton)
         NSLayoutConstraint.activate(
-            [
-                tableView.topAnchor.constraint(equalTo: topAnchor),
-                tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
-                tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
-                tableView.bottomAnchor.constraint(equalTo: bottomAnchor)]
+            dismissAllButton.anchor(top: nil, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor) +
+            tableView.anchor(top: topAnchor, leading: leadingAnchor, bottom: dismissAllButton.topAnchor, trailing: trailingAnchor)
         )
     }
 
